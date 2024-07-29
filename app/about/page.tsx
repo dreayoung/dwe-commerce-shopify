abd664676a6ec7eb0014a4fdcf751559a734f4ee
dreayoung <youngmandrea@gmail.com>
2024-07-29 11:27:02 -0400

    config webhooks

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,7 +11,7 @@ export default function AboutPage() {
               About
             </h1>
             <div className="font-hta text-2xl">
-              The only way you can truly help everyone, is to work on yourself The science of a
+              The only way you can truly help everyone, is to work on yourself. The science of a
               sound mind.
             </div>
             <p className="text-xs text-htf_green lg:text-lg">528 Hz</p>
